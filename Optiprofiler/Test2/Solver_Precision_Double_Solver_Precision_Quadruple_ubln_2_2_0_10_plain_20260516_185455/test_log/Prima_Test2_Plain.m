506c9059b0340c3c980a70765f573fae2658141e
Huangjw-Math <1144085911@qq.com>
2026-05-19 16:34:40 +0800

    Add files via upload

---
+++ Optiprofiler/Test2/Solver_Precision_Double_Solver_Precision_Quadruple_ubln_2_2_0_10_plain_20260516_185455/test_log/Prima_Test2_Plain.m
@@ -0,0 +1,14 @@
+function Prima_Test2_Plain()
+
+fprintf('Test 2:\n');
+fprintf('Solver1: Precision Double\n');
+fprintf('Solver2: Precision Quadruple\n');
+fprintf('Feature_Name: Plain\n');
+
+options.ptype = 'ubln';
+options.mindim = 2;
+options.maxdim = 2;
+options.feature_name = 'plain';
+
+scores = benchmark({@Solver_Precision_Double, @Solver_Precision_Quadruple}, options);
+end
